c787171cb26084cb33862d2ca75506fdcdc9a97a
Edward Ames <Edward.C.Ames@gmail.com>
2021-12-21 18:16:01 -0600

    Added Pet Owners page and controller.  Closes #15

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,10 +3,20 @@ package guru.springframework.sfgpetclinic.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
-    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String getVets() {
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listVets() {
         return "vets/index";
     }
 }
+/*
+@Controller
+public class VetController {
+    @RequestMapping({"/vets", "vets/index", "vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
+*/
